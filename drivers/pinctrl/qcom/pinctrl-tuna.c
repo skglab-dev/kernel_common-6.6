9541ce25891d231e73dd4a33ee0c67f9b7a3d0b7
nullptr03 <nullptr03@singkolab.my.id>
2026-03-10 15:37:19 +0800

    drivers: pinctrl: qcom: Fix incompatible function pointer types
    ../drivers/pinctrl/qcom/pinctrl-tuna.c:2321:12: error: incompatible function pointer types initializing 'int (*)(struct platform_device *)' with an expression of type 'void (struct platform_device *)' [-Wincompatible-function-pointer-types]
     2321 |         .remove = msm_pinctrl_remove,
          |                   ^~~~~~~~~~~~~~~~~~

--- drivers/pinctrl/qcom/pinctrl-tuna.c
+++ drivers/pinctrl/qcom/pinctrl-tuna.c
@@ -2318,7 +2318,7 @@ static struct platform_driver tuna_pinctrl_driver = {
 		.of_match_table = tuna_pinctrl_of_match,
 	},
 	.probe = tuna_pinctrl_probe,
-	.remove = msm_pinctrl_remove,
+	.remove_new = msm_pinctrl_remove,
 };
 
 static int __init tuna_pinctrl_init(void)
